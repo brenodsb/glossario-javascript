866767d1c16e573af0a1a41f053185012ab55844
Breno de Sousa Braga <brenosousabraga@gmail.com>
2024-04-11 11:09:22 -0300

    Novas paginas e funções adicionadas ao site

--- js/main.js
+++ js/main.js
@@ -13,4 +13,26 @@ onSelectChange = function() {
     $('#txt-desc').text(item.descricao);
 };
 
+register = function(){
+    var titulo = $('#txt-titulo').val();
+    var descricao = $('#txt-descricao').val();
+    
+    var dados = {
+        "id": db.data.length + 1,
+        "titulo": titulo,
+        "descricao": descricao
+    }
+
+    if (titulo && descricao) {
+        console.log(dados);
+        db.data.push(dados);
+        $('#txt-titulo').val('');
+        $('#txt-descricao').val('');
+        
+    }else{
+        console.error("Os novos dados devem conter id, titulo e descricao.");
+    }
+    
+}
+
 db.init(initSelect);
